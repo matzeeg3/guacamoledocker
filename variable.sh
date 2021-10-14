acf200f7914878ffeb1e7531242ea4edfe7257ef
Matthias <m.koesters@tec-networks.de>
2021-10-14 16:33:39 +0200

    asdf

--- variable.sh
+++ variable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pw=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
-sub='cat /tmp/sub'
-mail='cat /tmp/mail'
+sub=cat /tmp/sub
+mail=cat /tmp/mail
 sudo echo mail=$mail > /docker/config/.env
 sudo echo PW=$pw >> /docker/config/.env
 sudo echo sub=$sub >> /docker/config/.env
